4820d3440fc38d0b934eb5f611ae29d954041bda
vennakag <vennakag@student.touro.edu>
2026-01-15 00:34:24 -0500

    Sorting Algorithms

--- src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
+++ src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
@@ -7,5 +7,25 @@ public class InsertionSort implements Sorter {
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
         // TODO: implement insertion sort
+        for (int i = 1; i < a.length; i++) {
+            insertionSort(a,0,i,comp);
+        }
+    }
+    public <T> void insertionSort(T[] a, int start, int end, Comparator<? super T> comp) {
+        boolean finished = false;
+        int curr = end;
+        boolean moreToSearch = true;
+        while(moreToSearch && !finished) {
+            if (comp.compare(a[curr], a[curr - 1]) <= 0) {
+                T temp = a[curr];
+                a[curr] = a[curr - 1];
+                a[curr - 1] = temp;
+                curr--;
+                moreToSearch = (curr!=start);
+            }else{
+                finished = true;
+            }
+
+        }
     }
 }
